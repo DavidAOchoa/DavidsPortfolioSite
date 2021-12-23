1970489078459f2b1acd8bef5ad5db28c8880f8d
DavidAOchoa <od174140@gmail.com>
2021-12-22 18:51:57 -0600

    Added a header and footer to the site, also add bulma functionality

--- davids-portfolio-site/src/app/app.component.ts
+++ davids-portfolio-site/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-   Hello World!
-    <router-outlet></router-outlet>
+   <!-- Header -->
+   <app-header></app-header>
+    
+  <!-- routes get injected here-->
+  <router-outlet></router-outlet>
+
+  <!-- Footer -->
+  <app-footer></app-footer>
   `,
   styles: []
 })
