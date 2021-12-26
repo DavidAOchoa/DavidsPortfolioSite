227c43ea844ae3b7e371f1c272279515af63f03c
DavidAOchoa <Od174140@gmail.com>
2021-12-26 15:20:03 -0600

    add background to introductory text

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -12,25 +12,37 @@ import { Component, OnInit } from '@angular/core';
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Salsa&display=swap" rel="stylesheet">
   
-
-  <p class="title is-size-2">
-    Hi. I’m David.
-    A UI/UX Developer.  </p>
-    <p class="subtitle">
-    I’m a Student at the University of Texas at Dallas,
-    and part time Barista at XO Coffee & Bistro.
+  <div class="paragraphBody">
+    <p class="title is-size-2">
+      Hi. I’m David.
+      A UI/UX Developer.  
     </p>
+      
+    <p class="subtitle">
+      I’m a Student at the University of Texas at Dallas,
+      and part time Barista at XO Coffee & Bistro.
+    </p>
+  </div>
+    
   </div>
 </section>
   `,
   styles: [`
   
+    .paragraphBody {
+      background-color: rgba(31,27,34,.8);
+      color: white;
+      border-radius: 25px;
+      padding: 10px;
+      
+      
+    }
+
     .hero {
       background-image: url('/assets/image/SkullFaceGirl.jpg') !important;
       background-size: cover;
       background-position: center center;
-      font-family: 'Salsa', cursive;
-      
+      font-family: 'Salsa', cursive;    
     }
 
   `]
