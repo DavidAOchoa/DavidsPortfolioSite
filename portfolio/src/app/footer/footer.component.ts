2c1541a5bfe803de9193553af3ceb2e9927ef15c
DavidAOchoa <Od174140@gmail.com>
2021-12-25 19:00:46 -0600

    added buttons to header, and added a link to my github page

--- portfolio/src/app/footer/footer.component.ts
+++ portfolio/src/app/footer/footer.component.ts
@@ -4,14 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-footer',
   template: `
     <footer class="footer">
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+
+
      <div class="container content has-text-white">
-        <p>Made with ❤ by David from Scratch</p>
+        <p>Made with ❤ by David using Angular and Bulma.io</p>
      </div>
     </footer>
   `,
   styles: [`
   .footer {
     background-color: rgb(31,27,34);
+    font-family: 'Source Serif Pro', serif;
   }
 
   .container {
--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -1,34 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   template: `
    <div class="navbar">
       <!-- logo -->
       <div class="navbar-brand">
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+
         <a class="navbar-item">
           <img src="assets/image/ACMProjects.png" alt="Image not found">          
         </a> 
       </div>
 
       <div class="navbar-menu" id="nav-links">
-            <div class="navbar-start">
-              <a class="navbar-item">about</a>
-              <a class="navbar-item">resume</a>
-              <a class="navbar-item">work</a>
-              <a class="navbar-item">contact</a>
-            </div>
-          </div>
+        <div class="navbar-start">
+          <a class="navbar-item has-text-white">about</a>
+          <a class="navbar-item has-text-white">resume</a>
+          <a class="navbar-item has-text-white">work</a>
+          <a class="navbar-item has-text-white">contact</a>
+          <a class="navbar-item has-text-white"  href="https://github.com/DavidAOchoa">GitHub</a>
+          
+          
+          
+
+        </div>
+      </div>
 
     </div>
   `,
   styles: [`
-  .navbar-item {
- 
+
+  
+
+  .navbar-item:hover {
+    background-color: rgb(31,27,34);
   }
 
   .navbar {
     background-color: rgb(31,27,34);
+    font-family: 'Source Serif Pro', serif;
   }
   
   
